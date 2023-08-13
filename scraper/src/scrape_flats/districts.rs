6674202c563523a7df4e20ebfaab9719e162735c
Daniils Firgers <dfirger@gmail.com>
2023-08-13 10:35:13 +0300

    some chnages to parser ;pgic

--- scraper/src/scrape_flats/districts.rs
+++ scraper/src/scrape_flats/districts.rs
@@ -1,7 +1,15 @@
-// use reqwest::Error;
+use reqwest::StatusCode;
 
-const MAIN_URL: &str = "https://www.ss.com/en/real-estate/flats/riga/";
+const MAIN_URL: &str = "https://www.ss.com/en/real-estate/flats/riga/all/";
 
 pub async fn parse_list_of_districts() {
-    let mut _res = reqwest::get(MAIN_URL);
+    let client = reqwest::Client::new();
+    let mut res = client.get(MAIN_URL).send().await.unwrap();
+
+    let raw_html = match res.status() {
+        StatusCode::OK => res.text().await.unwrap(),
+        _ => panic!("Something went wrong"),
+    };
+
+    println!("HTML: {}", raw_html);
 }
