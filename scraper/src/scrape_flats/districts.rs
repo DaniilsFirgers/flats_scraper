e3f2fa9f4320f331b7503283b7331eb2d9614556
Daniils Firgers <dfirger@gmail.com>
2023-08-13 19:07:45 +0300

    additional refactoring

--- scraper/src/scrape_flats/districts.rs
+++ scraper/src/scrape_flats/districts.rs
@@ -13,11 +13,19 @@ pub async fn parse_list_of_districts() {
     };
     let document = Html::parse_document(&raw_html);
     let selector = Selector::parse("form#filter_frm>table>tbody").unwrap();
-    for (index, element) in document.select(&selector).enumerate() {
-        if index == 0 {
-            continue;
+    // Select the second tbody element (index 1)
+    if let Some(tbody_element) = document.select(&selector).nth(1) {
+        let tr_selector = Selector::parse("tr").unwrap();
+        let tr_elements = tbody_element.select(&tr_selector).collect::<Vec<_>>();
+
+        // Iterate through rows (tr elements) starting from index 1
+        for (index, tr_element) in tr_elements.iter().enumerate() {
+            let num_rows = tr_elements.len();
+            if index == 0 || index == num_rows - 1 {
+                continue; // Skip the first and last rows
+            }
+            let inner = tr_element.inner_html().to_string();
+            println!("Some data: {}", &inner);
         }
-        let inner = element.inner_html().to_string();
-        println!("Some shit {}", &inner);
     }
 }
