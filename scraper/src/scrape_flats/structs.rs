870d48da0392ebd6ab2073c902ec662c575451c2
Daniils Firgers <dfirger@gmail.com>
2023-08-15 23:03:55 +0300

    fix number parsing

--- scraper/src/scrape_flats/structs.rs
+++ scraper/src/scrape_flats/structs.rs
@@ -4,12 +4,12 @@ use serde::Serialize;
 pub struct Flat {
     description: String,
     street: String,
-    rooms: String,
-    square: String,
+    rooms: i32,
+    square: i32,
     floor: String,
     series: String,
-    square_m_price: String,
-    price: String,
+    square_m_price: i32,
+    price: i32,
 }
 impl Flat {
     // Create an empty Flat object
@@ -17,12 +17,12 @@ impl Flat {
         Flat {
             description: String::new(),
             street: String::new(),
-            rooms: String::new(),
-            square: String::new(),
+            rooms: 0,
+            square: 0,
             floor: String::new(),
             series: String::new(),
-            square_m_price: String::new(),
-            price: String::new(),
+            square_m_price: 0,
+            price: 0,
         }
     }
 
@@ -36,11 +36,13 @@ impl Flat {
     }
 
     pub fn set_rooms(&mut self, rooms: String) {
-        self.rooms = rooms;
+        let numeric_rooms = parse_string_to_numeric(rooms);
+        self.rooms = numeric_rooms;
     }
 
     pub fn set_square(&mut self, square: String) {
-        self.square = square;
+        let numeric_square = parse_string_to_numeric(square);
+        self.square = numeric_square;
     }
 
     pub fn set_floor(&mut self, floor: String) {
@@ -52,10 +54,25 @@ impl Flat {
     }
 
     pub fn set_square_m_price(&mut self, square_m_price: String) {
-        self.square_m_price = square_m_price;
+        let numeric_price = parse_string_to_numeric(square_m_price);
+        self.square_m_price = numeric_price;
     }
 
     pub fn set_price(&mut self, price: String) {
-        self.price = price;
+        let numeric_price = parse_string_to_numeric(price);
+        self.price = numeric_price;
     }
+}
+
+fn parse_string_to_numeric(value: String) -> i32 {
+    let cleaned_input: String = value
+        .chars()
+        .filter(|&c| c.is_digit(10) || c == ',')
+        .collect();
+    let parsed_int = cleaned_input.replace(",", "").parse::<i32>();
+    let numeric_price = match parsed_int {
+        Ok(price) => price,
+        Err(_) => 0,
+    };
+    numeric_price
 }
